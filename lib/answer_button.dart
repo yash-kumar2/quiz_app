5e0b6403e9c0e1592a6a17e73973b8a0cf41f295
yashsrepo <yashk6767@gmail.com>
2024-04-09 21:18:39 +0530

    navigating between different questions_screen

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -12,8 +12,19 @@ class AnswerButton extends StatelessWidget{
 
   Widget build(BuildContext context){
     return ElevatedButton(
-            onPressed:(){},
-            child: Text(answerText),
+             onPressed:onTap,
+            style:ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 40,
+              ),
+              backgroundColor: const Color.fromARGB(255, 79, 2, 92),
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius:BorderRadius.circular(40) ),
+            ),
+           
+            child: Text(answerText,textAlign: TextAlign.center,),
           );
   }
 
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
   @override
@@ -8,32 +9,38 @@ class QuestionsScreen extends StatefulWidget{
   }
 }
 class _QuestionsScreenState extends State<QuestionsScreen>{
+  var currentQuestionIndex=0;
+  void answerQuestion(){
+    setState(() {currentQuestionIndex++;});
+     
+  }
+
   @override
   Widget build(context){
+    final currentQuestion=questions[currentQuestionIndex];
+
     return SizedBox(
       width:double.infinity,
-       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:[
-          const Text('Questipns',
-          style: TextStyle(
-            color: Colors.white,
-          ),),
-          const SizedBox(height: 30),
-          AnswerButton(answerText: "answer 1",
-           onTap: (){},
-           ),
-           AnswerButton(answerText: "answer 2",
-           onTap: (){},
-           ),
-           AnswerButton(answerText: "answer 3",
-           onTap: (){},
-           ),
-           AnswerButton(answerText: "answer 4",
-           onTap: (){},
-           ),
-        ]
-      ),
+       child: Container(
+        margin: EdgeInsets.all(40),
+         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            Text(currentQuestion.text,
+            style: TextStyle(
+              color:  Colors.white,
+            ),
+            textAlign: TextAlign.center,),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer){
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+            }),
+           
+             
+          ]
+               ),
+       ),
     );
   }
 }
